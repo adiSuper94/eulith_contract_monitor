345303bec4000b494edd58cb8b1725e1574a05ea
Aditya Subramanian <aditarun2008@gmail.com>
2023-02-02 22:13:31 -0500

    cleanup folder name

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/contarcts/Counter.sol";
+import "../src/contracts/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
