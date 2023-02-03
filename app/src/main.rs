9c111eb1ca4a91313919cf155efcf23760b887cb
Aditya Subramanian <aditarun2008@gmail.com>
2023-02-03 04:15:21 -0500

    ether rs is such a pain

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,16 @@
 use bindings::counter::Counter;
+use ethers::{prelude::*, utils::Anvil};
+use std::sync::Arc;
 
+const RPC_URL: &str = "http://localhost:8545";
 #[tokio::main]
-async fn main() {
-    async_fn().await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let provider = Provider::<Http>::try_from(RPC_URL)?;
+    let provider = Arc::new(provider);
+    let counter = Counter::from(counter);
+    Ok(())
 }
 
-async fn async_fn() {
+async fn publish_counter_contract() {
     println!("Hello, world!");
 }
